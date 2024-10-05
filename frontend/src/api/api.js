26cc51d21c82e7fae43d7ce49f9a4b72905200ca
kanjiya paras <115352713+kanjiyaparas@users.noreply.github.com>
2024-10-06 04:21:57 +0530

    Update api.js

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create an Axios instance
 const api = axios.create({
-  baseURL: "http://localhost:4000/api", // Make sure this matches your backend URL
+  baseURL: "https://event-management-aq3j.onrender.com/api", // Make sure this matches your backend URL
 });
 
 // Add a request interceptor to include token in requests (if necessary)
